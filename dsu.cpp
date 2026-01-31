773d87ee58f83ecb6ea6a186bbf132082f356316
Avinash Singh <avinashsingh122006@gmail.com>
2026-01-31 09:38:44 +0530

    Refactor DSU structure and initialization

--- dsu.cpp
+++ dsu.cpp
@@ -1,22 +1,18 @@
 struct DSU {
     vi parent, rank;
-
     DSU(int n) {
         parent.resize(n);
         rank.resize(n);
         for (int i = 0; i < n; i++) make_set(i);
     }
-
     void make_set(int a) {
         parent[a] = a;
         rank[a] = 0;
     }
-
     int find_set(int a) {
         if (a == parent[a]) return a;
         return parent[a] = find_set(parent[a]);
     }
-
     void union_sets(int a, int b) {
         int par_a = find_set(a);
         int par_b = find_set(b);
