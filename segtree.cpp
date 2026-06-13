16ddd5f11cb9a18928950b612e5bb0cdd87f8871
Avinash Singh <avinashsingh122006@gmail.com>
2026-06-13 15:46:53 +0530

    Refactor segtree struct to SegTree class

--- segtree.cpp
+++ segtree.cpp
@@ -1,58 +1,55 @@
-struct segtree {
+class SegTree {
+private:
+    int n;
+    vector<int> tree;
 
-    int size, no_op = 0;
-    vector<int> values;
-
-    segtree(int n) {
-        size = 1;
-        while (size < n) size *= 2;
-        values.assign(2 * size, no_op); 
-    }
-
-    void build(vector<int> &a, int x, int lx, int rx) {
-        if (rx - lx == 1) {
-            if (lx < (int)a.size()) values[x] = a[lx];
-            return;
-        }
-        int m = (lx + rx) / 2;
-        build(a, 2 * x + 1, lx, m);
-        build(a, 2 * x + 2, m, rx);
-        values[x] = merge(values[2 * x + 1], values[2 * x + 2]);
-    }
-
-    void build(vector<int> &a) {
-        build(a, 0, 0, size);
-    }
-
+    // change this according to the problem
+    int identity = 0;
     int merge(int a, int b) {
         return a + b;
     }
 
-    void set(int i, int b, int x, int lx, int rx) {
-        if (rx - lx == 1) {
-            values[x] = b;
+    void build(int node, int l, int r, vector<int> &a) {
+        if (l == r) {
+            tree[node] = a[l];
             return;
         }
-        int m = (lx + rx) / 2;
-        if (i < m) set(i, b, 2 * x + 1, lx, m);
-        else set(i, b, 2 * x + 2, m, rx);
-        values[x] = merge(values[2 * x + 1], values[2 * x + 2]);
+        int m = (l + r) / 2;
+        build(2 * node, l, m, a);
+        build(2 * node + 1, m + 1, r, a);
+        tree[node] = merge(tree[2 * node], tree[2 * node + 1]);
     }
 
-    void set(int i, int x) {
-        set(i, x, 0, 0, size);
+    int query(int node, int l, int r, int ql, int qr) {
+        if (l > qr || r < ql) return identity;
+        if (ql <= l && qr >= r) return tree[node];
+        int m = (l + r) / 2;
+        int left = query(2 * node, l, m, ql, qr);
+        int right = query(2 * node + 1, m + 1, r, ql, qr);
+        return merge(left, right);
     }
 
-    int calc(int l, int r, int x, int lx, int rx) {
-        if (lx >= r || l >= rx) return 0;
-        if (l <= lx && r >= rx) return values[x];
-        int m = (lx + rx) / 2;
-        int value_left = calc(l, r, 2 * x + 1, lx, m);
-        int value_right = calc(l, r, 2 * x + 2, m, rx);
-        return merge(value_left, value_right);
+    void update(int node, int l, int r, int idx, int val) {
+        if (l == r) {
+            tree[node] = val;
+            return;
+        }
+        int m = (l + r) / 2;
+        if (idx <= m) update(2 * node, l, m, idx, val);
+        else update(2 * node + 1, m + 1, r, idx, val);
+        tree[node] = merge(tree[2 * node], tree[2 * node + 1]);
     }
 
-    int calc(int l, int r) {
-        return calc(l, r, 0, 0, size);
+public:
+    SegTree(vector<int> &a) {
+        n = a.size();
+        tree.assign(4 * n, identity);
+        build(1, 0, n - 1, a);
+    }
+    int query(int l, int r) {
+        return query(1, 0, n - 1, l, r);
+    }
+    void update(int idx, int val) {
+        update(1, 0, n - 1, idx, val);
     }
 };
